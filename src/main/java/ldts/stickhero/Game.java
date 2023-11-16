a18de7fa9057845175af79d84c81acc36d4e2768
jpnsantoss <jpnsantos14@gmail.com>
2023-11-16 12:31:48 +0000

    started screen implementation

---
+++ src/main/java/ldts/stickhero/Game.java
@@ -0,0 +1,15 @@
+package ldts.stickhero;
+
+import ldts.stickhero.gui.LanternaGUI;
+
+public class Game {
+    private final LanternaGUI gui;
+
+    public Game() {
+        this.gui = new LanternaGUI();
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
