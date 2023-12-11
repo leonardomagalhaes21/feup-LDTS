70f78050966c660c6ddb7e9c31fe2a79567e6132
leonardomagalhaes21 <leonardomagalhaess21@gmail.com>
2023-12-11 22:55:44 +0000

    Top Scores and Instructions menu

--- src/main/java/ldts/dino/model/menu/ScoresMenu.java
+++ src/main/java/ldts/dino/model/menu/ScoresMenu.java
@@ -1,10 +1,24 @@
 package ldts.dino.model.menu;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class ScoresMenu extends Menu {
 
     public ScoresMenu() {
-        this.entries = Arrays.asList("1 - 5000", "2 - 4000", "3 - 2");
+        this.entries = new ArrayList<>();
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("src/main/resources/scores/topscore.txt"));
+            String line = reader.readLine();
+            while (line != null) {
+                this.entries.add(line);
+                line = reader.readLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
