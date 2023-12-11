4490c90c1682f5e165c376825f17fdcc783d3aaa
jpnsantoss <jpnsantos14@gmail.com>
2023-12-11 15:50:31 +0000

    Shield + coin

--- src/main/java/ldts/dino/model/game/elements/collectables/Collectable.java
+++ src/main/java/ldts/dino/model/game/elements/collectables/Collectable.java
@@ -1,12 +1,13 @@
 package ldts.dino.model.game.elements.collectables;
 
+import ldts.dino.model.game.Game;
 import ldts.dino.model.game.elements.Element;
 import ldts.dino.utils.Position;
 
 public abstract class Collectable extends Element {
     private final int speed = 5;
 
-    public Collectable(Position position, int width, int height) {
+    public Collectable( Position position, int width, int height) {
         super(position, width, height);
     }
 
@@ -17,4 +18,6 @@ public abstract class Collectable extends Element {
     public void move() {
         this.getPosition().setX(this.getPosition().getX() - getSpeed());
     }
+
+    public abstract void consume(Game game);
 }
